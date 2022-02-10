44edfd23f3abacafa71527acb5f8d5028f5b7dae
PabloJuice <pawlo.dzhus@gmail.com>
2022-02-10 15:32:51 +0200

    initial commit

---
+++ src/main/java/taskOne/Calculator.java
@@ -0,0 +1,8 @@
+package taskOne;
+
+public class Calculator {
+	public static void main(String[] args) {
+		//Task 1. Implement a simple calculator with 4 operations.
+		// Each operation - different method. Use it in the main method.
+	}
+}
---
+++ src/main/java/taskOne/StringCalculator.java
@@ -0,0 +1,17 @@
+package taskOne;
+
+public class StringCalculator {
+	public static void main(String[] args) {
+		//Task 2. Given a string, find the number of words in it.
+		String s = "Task 2. Given a string, find the number of words in it.";
+		System.out.println(s.split(" ").length);
+
+		//Task 3. Given a string, show number of chars in each word
+		for(String ss : s.split())
+
+		//Task 4. Create function which takes 2 parameters:
+		//1st(String) – text; 2nd (int) - index of word.
+		// Function should return number of chars in word by its index in text
+
+	}
+}
